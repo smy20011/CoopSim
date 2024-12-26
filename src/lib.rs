dc59a39058eaa38d8a7516a8c9d9acdbcc87c201
Siyuan Ma <smy20011@gmail.com>
2024-12-26 00:07:03 -0800

    Add Support for Termnial Animation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, usize};
+use std::{
+    collections::{BTreeMap, HashMap},
+    usize,
+};
 
 use rand::{seq::SliceRandom, thread_rng};
 
@@ -8,7 +11,7 @@ pub enum Action {
     Deflect,
 }
 
-#[derive(Clone, Copy, Eq, PartialEq)]
+#[derive(Clone, Copy, Eq, PartialEq, Debug, Hash, PartialOrd, Ord)]
 pub enum Strategy {
     Deflect,
     TicToc,
@@ -33,6 +36,7 @@ impl Strategy {
 
 type Coord = (usize, usize);
 
+#[derive(Clone, Debug)]
 pub struct Agent {
     coord: Coord,
     history: HashMap<Coord, Vec<Action>>,
@@ -65,10 +69,10 @@ impl Agent {
         } else {
             self.history.insert(agnet.coord, vec![other_action]);
         }
-        self.score += score;
+        self.score = self.score * 1.0 + score;
     }
 
-    fn new(coord: Coord, strategy: Strategy) -> Agent {
+    pub fn new(coord: Coord, strategy: Strategy) -> Agent {
         Agent {
             coord,
             history: HashMap::new(),
@@ -76,11 +80,19 @@ impl Agent {
             score: 0.0,
         }
     }
+
+    fn random(coord: Coord, strategies: &Vec<Strategy>) -> Agent {
+        let mut rng = thread_rng();
+        Agent::new(coord, strategies.choose(&mut rng).unwrap().to_owned())
+    }
 }
 
+#[derive(Debug, Clone)]
 pub struct Metric {
-    coop_agents: i32,
-    coop_actions: i32,
+    pub strategies: BTreeMap<Strategy, usize>,
+    pub max_score: BTreeMap<Strategy, f32>,
+    pub coop_actions: i32,
+    pub snapshot: Vec<Vec<Strategy>>,
 }
 
 pub struct Environment {
@@ -108,18 +120,54 @@ impl Environment {
             }
         });
 
-        let mut coop_agents = 0;
-        self.for_each_cell(|curr, _| {
-            if curr.strategy == Strategy::Coop {
-                coop_agents += 1;
-            }
+        let mut strategies: BTreeMap<Strategy, usize> = BTreeMap::new();
+        let mut max_score: BTreeMap<Strategy, f32> = BTreeMap::new();
+        self.grid.iter().for_each(|curr| {
+            let count = strategies.get(&curr.strategy).cloned().unwrap_or(0) + 1;
+            strategies.insert(curr.strategy, count);
+
+            let score = max_score.get(&curr.strategy).cloned().unwrap_or(0.0);
+            max_score.insert(curr.strategy, score.max(curr.score));
         });
+        let snapshot: Vec<Vec<Strategy>> = self
+            .grid
+            .iter()
+            .map(|a| a.strategy)
+            .collect::<Vec<Strategy>>()
+            .chunks(self.num_col)
+            .map(|c| c.iter().cloned().collect())
+            .collect();
 
         let coop_actions = actions.values().filter(|a| (**a == Action::Coop)).count() as i32;
 
         Metric {
             coop_actions,
-            coop_agents,
+            strategies,
+            max_score,
+            snapshot,
+        }
+    }
+
+    pub fn new(num_row: usize, num_col: usize) -> Environment {
+        let strategies = vec![Strategy::Deflect, Strategy::TicToc];
+        Environment::new_with_agent_func(num_row, num_col, |c| Agent::random(c, &strategies))
+    }
+
+    pub fn new_with_agent_func<F>(num_row: usize, num_col: usize, mut agent_fn: F) -> Environment
+    where
+        F: FnMut(Coord) -> Agent,
+    {
+        let mut grid: Vec<Agent> = Vec::with_capacity(num_row * num_col);
+        for i in 0..num_row {
+            for j in 0..num_col {
+                grid.push(agent_fn((i, j)));
+            }
+        }
+
+        Environment {
+            num_row,
+            num_col,
+            grid,
         }
     }
 
@@ -208,5 +256,30 @@ mod tests {
             assert_eq!(Strategy::Coop.get_action(&history), Action::Coop);
             assert_eq!(Strategy::Deflect.get_action(&history), Action::Deflect);
         }
+    }
+
+    #[test]
+    fn test_agent() {
+        let mut agent = Agent::new((0, 0), Strategy::TicToc);
+        let mut other_agent = Agent::new((0, 1), Strategy::Deflect);
+
+        assert_eq!(agent.get_action(&other_agent), Action::Coop);
+        assert_eq!(other_agent.get_action(&agent), Action::Deflect);
+
+        agent.score(&other_agent, Action::Deflect, 0.0);
+        other_agent.score(&agent, Action::Coop, 3.0);
+
+        assert_eq!(agent.get_action(&other_agent), Action::Deflect);
+        assert_eq!(other_agent.get_action(&agent), Action::Deflect);
+
+        agent.adapt(vec![&other_agent]);
+        assert_eq!(agent.strategy, Strategy::Deflect);
+    }
+
+    #[test]
+    fn test_env() {
+        let mut env = Environment::new(50, 50);
+        let result = env.step();
+        assert!(result.coop_actions < 1400 * 8 && result.coop_actions > 1100 * 8);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+use std::{fmt::Debug, thread::sleep, time::Duration};
+
+use coop::{Agent, Environment, Metric, Strategy};
+use rand::{thread_rng, Rng};
+use ratatui::{
+    style::{Color, Stylize},
+    text::Line,
+    widgets::{Paragraph, Widget},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut env = Environment::new_with_agent_func(50, 50, |c| {
+        let rand: f32 = thread_rng().gen();
+        let prob_coop = 0.50;
+        let prob_random = 0.00;
+        let prob_tic = 0.0;
+
+        let strategy = if rand < prob_coop {
+            Strategy::Coop
+        } else if rand < prob_coop + prob_random {
+            Strategy::Random
+        } else if rand < prob_coop + prob_random + prob_tic {
+            Strategy::TicToc
+        } else {
+            Strategy::Deflect
+        };
+        Agent::new(c, strategy)
+    });
+
+    color_eyre::install().unwrap();
+    let mut term = ratatui::init();
+
+    for step in 1..1000 {
+        let result = env.step();
+        term.draw(|frame| {
+            frame.render_widget(strategy_canvas(step, result), frame.area());
+        })
+        .unwrap();
+        sleep(Duration::from_millis(10));
+    }
+
+    ratatui::restore();
+}
+
+fn strategy_color(strategy: Strategy) -> Color {
+    match strategy {
+        Strategy::Deflect => Color::Red,
+        Strategy::Coop => Color::Green,
+        Strategy::TicToc => Color::Yellow,
+        Strategy::Random => Color::Magenta,
+    }
+}
+
+fn strategy_canvas(step: usize, metric: Metric) -> impl Widget {
+    let status_line = Line::from(format!(
+        "Step: {} Agents: {:?} Score: {:?}",
+        step, metric.strategies, metric.max_score
+    ));
+    let mut lines: Vec<Line> = metric
+        .snapshot
+        .iter()
+        .map(|row| Line::from_iter(row.iter().map(|s| "██".fg(strategy_color(*s)))))
+        .collect();
+    lines.push(status_line);
+    Paragraph::new(lines)
 }
